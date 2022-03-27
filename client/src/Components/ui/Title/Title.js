9d188d127433e150aecc06df7992807aa9402d21
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-27 16:23:16 +0100

    adds test for Title component

--- client/src/Components/ui/Title/Title.js
+++ client/src/Components/ui/Title/Title.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Title = ({ header, subheader }) => (
   <div>
-    <h1>{header}</h1>
-    <h2>{subheader}</h2>
+    <h1 data-test='header-text'>{header}</h1>
+    <h2 data-test='subheader-text'>{subheader}</h2>
   </div>
 )
 
