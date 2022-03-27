647b1a6ee17954d9749924a2cbbc68bf786c4492
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-27 14:25:33 +0100

    refactors api calls to catch errors

--- api/server.js
+++ api/server.js
@@ -10,21 +10,29 @@ app.get('/', (req, res) => {
   res.send(`Hey! I am running on port ${port} ;D `)
 })
 
-app.get('/pokemon/:name', async (req, res, next) => {
-
-  const[firstRequest, secondRequest] = await axios.all([
+app.get('/pokemon/:name', (req, res, next) => {
+  axios.all([
     axios.get(`https://pokeapi.co/api/v2/pokemon-species/${req.params.name}`),
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.name}`),
-   ]);
-   const thirdRequest = await axios.get(encodeURI(`https://api.funtranslations.com/translate/shakespeare.json?text=${firstRequest.data.flavor_text_entries[1].flavor_text.replace(/\n/g, ' ')}`
-   ))
-   res.send({
-    "name": firstRequest.data.name,
-    "description": thirdRequest.data.contents.translated,
-    "image": secondRequest.data.sprites.front_default
-   })
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.name}`)
+  ])
+  .then(axios.spread((responseOne, responseTwo) => {
+    const firstResponse = responseOne
+    const secondResponse = responseTwo
+    const pokemonDescription = responseOne.data.flavor_text_entries[1].flavor_text.replace(/\n/g, ' ')
+  axios.get(encodeURI(`https://api.funtranslations.com/translate/shakespeare.json?text=${pokemonDescription}`))
+    .then(response => {
+      const shakespeareTranslation = response.data.contents.translated
+      res.status(200)
+      res.send({
+        "name": firstResponse.data.name,
+        "description": shakespeareTranslation,
+        "image": secondResponse.data.sprites.front_default
+        })
+      })
+    }
+  ))
+  .catch(err => next(err));
 })
-
 
 app.listen(port, () => {
 console.log(`listening at http://localhost:${port}`)
