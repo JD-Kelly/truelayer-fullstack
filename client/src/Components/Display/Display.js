38dab134032e9c9814d4585cb7e16ad5b77afdbd
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-27 13:22:16 +0100

    adds loading handler

--- client/src/Components/Display/Display.js
+++ client/src/Components/Display/Display.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import LoadingHandler from "../ui/LoadingHandler/LoadingHandler"
 
 const Display = ({ isError ,data, isLoading}) => {
   return (
     isError ? <p>There was an error</p> :
-    isLoading ? <p>is Loading...</p> :
+    isLoading ? <LoadingHandler /> :
       <>
         <h3>{data.name}</h3>
         <h3>{data.description}</h3>
---
+++ client/src/Components/ui/LoadingHandler/LoadingHandler.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingHandler = () => {
+  return (
+    <div>Loading...</div>
+  )
+}
+
+export default LoadingHandler
