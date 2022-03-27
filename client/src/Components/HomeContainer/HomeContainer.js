b38cfed48aa62e118ba562af9052c27031c05ff2
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-27 12:41:27 +0100

    Adds a HomeContainer

---
+++ client/src/Components/HomeContainer/HomeContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from '../Form/Form'
+
+const HomeContainer = () => {
+  return (
+    <>
+      <h1>Pokemon Search Engine</h1>
+      <Form />
+    </>
+  )
+}
+
+export default HomeContainer
