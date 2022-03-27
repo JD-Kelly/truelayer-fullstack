307d35f48ff41b6b2fc24fafefce8bec502ba761
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-27 16:31:44 +0100

    test for submit button rendering

--- client/src/Components/ui/Button/Button.js
+++ client/src/Components/ui/Button/Button.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Button = ({ isDisabled, children, type, onClick, }) => (
   <button
+    data-test={'submit-button'}
     disabled={isDisabled}
     type="button"
     onClick={onClick}
