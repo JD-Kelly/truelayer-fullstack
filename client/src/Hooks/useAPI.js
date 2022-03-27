829e1c4b599840d5f94456a0008b3a05ddfd093a
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-27 11:20:09 +0100

    sets isLoading state for data retrieval

--- client/src/Hooks/useAPI.js
+++ client/src/Hooks/useAPI.js
@@ -3,20 +3,25 @@ import axios from 'axios';
 
 export const useFetchData = (pokemonName) => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = () => {
+    setIsLoading(true)
     axios
       .get(`http://localhost:9000/pokemon/${pokemonName}`)
       .then((response) => {
         setData(response.data)
+        setIsLoading(false)
         console.log(data)
       })
       .catch((error) => {
+        setIsLoading(false)
         console.log(error)
       })
   };
   return {
     fetchData,
     data,
+    isLoading
   };
 }
