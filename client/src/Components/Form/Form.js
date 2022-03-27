a2c812a73591e9d243a55adc9fdfcde2d3ce7a0b
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-27 11:16:19 +0100

    sets input validation state

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -5,6 +5,7 @@ import { useFetchData } from "../../Hooks/useAPI";
 const Form = () => {
 
   const [pokemonName, setPokemonName] = useState("");
+  const [validationError, setValidationError] = useState(false);
 
   const {fetchData, data } = useFetchData(pokemonName)
 
@@ -14,12 +15,14 @@ const Form = () => {
     const regex = /^(?![\s.]+$)[a-zA-Z\s.']*$/
     if (pokemonName.match(regex)) {
       setPokemonName(pokemonName)
+      setValidationError(false)
     } else {
-      console.log("Please enter a valid pokemon name")
+      setValidationError(true)
     }
   }
 
   const handleOnClick = () => {
+    setValidationError(false)
     fetchData()
     setPokemonName('')
     console.log(pokemonName)
@@ -27,6 +30,7 @@ const Form = () => {
 
   return (
     <div>
+       {validationError && <h3>Please enter a valid input</h3>}
         <input
           type="text"
           value={pokemonName}
