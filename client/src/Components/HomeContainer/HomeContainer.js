1a91bb503953ece2a6630e3baf3e5f32282806de
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-27 14:44:11 +0100

    adds a page title component

--- client/src/Components/HomeContainer/HomeContainer.js
+++ client/src/Components/HomeContainer/HomeContainer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Form from '../Form/Form'
+import Title from '../ui/Title/Title'
 
 const HomeContainer = () => {
   return (
     <>
-      <h1>Pokemon Search Engine</h1>
+      <Title header="Pokédex" subheader='Search for your favourite Pokémon' />
       <Form />
     </>
   )
---
+++ client/src/Components/ui/Title/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Title = ({ header, subheader }) => (
+  <div>
+    <h1>{header}</h1>
+    <h2>{subheader}</h2>
+  </div>
+)
+
+export default Title
