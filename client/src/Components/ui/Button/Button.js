ed25de19f45264f22fd18b4d1a7f0be64614daaf
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-28 08:04:10 +0100

    adds styling to Button component

--- client/src/Components/ui/Button/Button.js
+++ client/src/Components/ui/Button/Button.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { StyledButton } from './Button.styles'
 
 const Button = ({ isDisabled, children, type, onClick, }) => (
-  <button
+  <StyledButton
     data-test={'submit-button'}
     disabled={isDisabled}
     type="button"
     onClick={onClick}
   >
     {children}
-  </button>
+  </StyledButton>
 );
 
 export default Button;
