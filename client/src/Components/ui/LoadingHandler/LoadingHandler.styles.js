f09821425eade9e9b258a60bbd0a99e38b9b26c1
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-29 19:20:00 +0100

    styles loading handler

---
+++ client/src/Components/ui/LoadingHandler/LoadingHandler.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+export const StyledLoadingHandler = styled.div`
+  font-size: 20px;
+  margin-top: 30px;
+  margin-bottom: 10px;
+@media screen and (max-width: 800px) {
+  font-size: 15px;
+  text-align: center;
+}
+`
