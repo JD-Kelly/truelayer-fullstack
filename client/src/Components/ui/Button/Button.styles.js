8b3561b4d422f25787c764b8664b4ffe918bba98
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-29 19:22:07 +0100

    refactors button styling

--- client/src/Components/ui/Button/Button.styles.js
+++ client/src/Components/ui/Button/Button.styles.js
@@ -1,20 +1,5 @@
 import styled from "styled-components";
+import { GlobalStyledButton } from "../../../styles/global";
 
-export const StyledButton = styled.button`
-  background: #196cff;
-  color: #ffff;
-  width: 150px;
-  font-size: 15px;
-  font-weight: 600;
-  border-radius: 3px;
-  padding: 18px 50px;
-  border: none;
-  cursor: pointer;
-  &:hover {
-  background: #0000ee;
-  transition: 0.5s;
-  }
-  @media screen and (max-width: 800px) {
-   padding: 10px 40px;
-  }
+export const StyledButton = styled(GlobalStyledButton)`
 `;
--- client/src/Components/ui/ErrorHandler/ErrorHandler.styles.js
+++ client/src/Components/ui/ErrorHandler/ErrorHandler.styles.js
@@ -1,29 +1,16 @@
 import styled from "styled-components";
+import { GlobalStyledButton } from "../../../styles/global";
+
 
 export const StyledErrorHandler = styled.div`
-font-size: 20px;
-margin-top: 30px;
-margin-bottom: 10px;
+  font-size: 20px;
+  margin-top: 30px;
+  margin-bottom: 10px;
 @media screen and (max-width: 800px) {
   font-size: 15px;
   text-align: center;
 }
 `
-export const StyledRetryButton = styled.button`
-background: #5b5b5b;
-  color: #ffff;
-  width: 150px;
-  font-size: 15px;
-  font-weight: 600;
-  border-radius: 3px;
-  padding: 18px 50px;
-  border: none;
-  cursor: pointer;
-  &:hover {
-  background: #898888;
-  transition: 0.5s;
-  }
-  @media screen and (max-width: 800px) {
-   padding: 10px 40px;
-  }
-  `
+export const StyledRetryButton = styled(GlobalStyledButton)`
+  color: red;
+`
