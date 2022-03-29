6f6c7f68bbf76c6bca02e0fcda6638c53109f03f
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-29 19:19:47 +0100

    adds global styles

--- client/src/styles/global.js
+++ client/src/styles/global.js
@@ -16,3 +16,22 @@ export const ValidationWarning = styled.div`
   text-align: center;
   color: red;
 `;
+
+export const GlobalStyledButton = styled.button`
+  background: #196cff;
+  color: #ffff;
+  width: 150px;
+  font-size: 15px;
+  font-weight: 600;
+  border-radius: 3px;
+  padding: 18px 50px;
+  border: none;
+  cursor: pointer;
+  &:hover {
+  background: #0000ee;
+  transition: 0.5s;
+  }
+  @media screen and (max-width: 800px) {
+   padding: 10px 40px;
+  }
+`;
