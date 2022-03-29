ac0e39756995c4d6d03ce9538b941128bffb0491
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-29 12:14:22 +0100

    minor styling fixes for font size

--- client/src/Components/ui/Title/Title.styles.js
+++ client/src/Components/ui/Title/Title.styles.js
@@ -8,16 +8,16 @@ export const TitleContainer = styled.div`
 `
 
 export const Header = styled.div`
-  font-size: 45px;
+  font-size: 65px;
   margin-top: 50px;
   @media screen and (max-width: 800px) {
-    font-size: 25px;
+    font-size: 35px;
     text-align: center;
   }
 `
 
 export const SubHeader = styled.div`
-  font-size: 20px;
+  font-size: 25px;
   margin-top: 30px;
   margin-bottom: 10px;
   @media screen and (max-width: 800px) {
