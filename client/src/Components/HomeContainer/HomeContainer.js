3539df46a87f15363c79ec92786f0e78722a5883
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-29 19:22:46 +0100

    refactors home container

--- client/src/Components/HomeContainer/HomeContainer.js
+++ client/src/Components/HomeContainer/HomeContainer.js
@@ -2,13 +2,11 @@ import React from 'react'
 import Form from '../Form/Form'
 import Title from '../ui/Title/Title'
 
-const HomeContainer = () => {
-  return (
+const HomeContainer = () => (
     <>
       <Title header="Pokédex" subheader='Search for your favourite Pokémon' />
       <Form />
     </>
   )
-}
 
 export default HomeContainer
