17c2d8fe7aa836cd44e057f4eb50871d03e1679d
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-30 07:51:44 +0100

    minor formatting

--- client/testUtils.js
+++ client/testUtils.js
@@ -1,7 +1,8 @@
-import { configure } from '@testing-library/react'
+import { configure } from "@testing-library/react"
+import '@testing-library/jest-dom';
 
-export * from '@testing-library/react'
+export * from "@testing-library/react"
 
 configure({
-  testIdAttribute: 'data-test'
+  testIdAttribute: "data-test"
 })
